83bac2c83d33429ba3157b3b63519d135c333c22
Renz <johnrenztolentino1028@gmail.com>
2025-05-30 14:14:31 +0800

    update

--- components/ui/loader.tsx
+++ components/ui/loader.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react"
 export function Loader() {
   return (
     <div className="flex items-center justify-center">
-      <Loader2 className="h-8 w-8 animate-spin text-primary" />
+      <Loader2 className="h-8 w-8 animate-spin text-black/80" />
     </div>
   )
 }
